cdd7d0c4650267b3a7545af8c19b1709f1d03e28
Etnath <kevin.giffone@gmail.com>
2016-12-04 17:52:36 +0100

    Transfer from Greenround.Portable.Utilities

---
+++ Etnath.Utilities.Portable/Etnath.Utilities.Portable/Helper/EnumHelper.cs
@@ -0,0 +1,44 @@
+﻿using Etnath.Utilities.Portable.Attribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etnath.Utilities.Portable.Helper
+{
+    public static class EnumHelper
+    {
+        public static string GetDescription(this Enum value)
+        {
+            return GetDescriptionString(value);
+        }
+
+        public static string GetDescriptionString(Enum value)
+        {
+            Type type = value.GetType();
+            string enumName = Enum.GetName(type, value);
+            if (enumName != null)
+            {
+                FieldInfo field = type.GetRuntimeFields().First(f => f.Name == enumName);
+                field.GetCustomAttribute<DescriptionAttribute>();
+                DescriptionAttribute attribute = field.GetCustomAttribute<DescriptionAttribute>();
+                if (attribute != null)
+                {
+                    return attribute.Description;
+                }
+                else
+                {
+                    //TODO throw a more meaningful exception
+                    throw new Exception();
+                }
+            }
+            else
+            {
+                //TODO throw a more meaningful exception
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Etnath.Utilities.Portable/Etnath.Utilities.Portable/Patterns/SingletonBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etnath.Utilities.Portable.Patterns
+{
+    /// <summary>
+    /// Inherit from this class to ensure only one instance of a given class is instanciated
+    /// </summary>
+    /// <typeparam name="T">The class that must be a singleton</typeparam>
+    public class SingletonBase<T> where T : class
+    {
+        /// <summary>
+        /// The singleton instance. the default contructor of the class T is used to build the instance.
+        /// </summary>
+        private static readonly Lazy<T> _instance = new Lazy<T>(() => Activator.CreateInstance<T>());
+
+        /// <summary>
+        /// Return the singleton instance.
+        /// </summary>
+        public static T Instance
+        {
+            get { return _instance.Value; }
+        }
+    }
+}
